6d1e94c1f12401a4118126862ed8339ff30cea12
rana.lubis <rana.lubis@gmail.com>
2021-12-03 08:06:35 +0700

    bsi proxy

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+const fetch = require("node-fetch");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use('/api', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
+
+app.all("/bsiSandbox", (req, res) => {
+  console.log(typeof req.body);
+
+  fetch("https://sandbox.api.bpi.co.id/ext/bnis/?fungsi=vabilling", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req.body),
+  })
+    .then((result) => result.json())
+    .then((result) => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+app.all("/bsiProd", (req, res) => {
+  console.log(typeof req.body);
+
+  fetch("https://api.bpi.co.id/ext/bnis/?fungsi=vabilling", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req.body),
+  })
+    .then((result) => result.json())
+    .then((result) => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+app.listen(3008, () => {
+  console.log("app is running");
+});
